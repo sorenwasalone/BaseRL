050ecb95dd86aff570d05ec23fde3b4bcf596cc6
Soren <sociopathecy@gmail.com>
2016-04-19 21:17:23 +0200

    Now with movement!

--- main.c
+++ main.c
@@ -7,22 +7,13 @@
 int imap[2][MAP_MAX][MAP_MAX]={0}; // Item Map -> stores item data for each block (items,y,x)
 int dmap[DMAP_AMAX][MAP_MAX][MAP_MAX]={0}; // Data Map -> determines how the block functions (data,y,x)
 char vmap[MAP_MAX][MAP_MAX]={0}; // Visual Map -> shows the block (x,y)
-char ch;
 int px = 0;
 int py = 0;
-
-void d_test_imap(){
-	for(int i = 0; i < MAP_MAX; i++){
-		for(int ii = 0; ii < MAP_MAX; ii++){
-			imap[0][i][ii]=0;
-		}
-	}
-}
 
 void p_test_imap(){
 	for(int i = 0; i < MAP_MAX; i++){
 		for(int ii = 0; ii < MAP_MAX; ii++){
-			imap[0][i][ii]=0;
+			imap[0][i][ii]='.';
 			mvprintw(i,ii,"%d",imap[0][i][ii]);
 		}
 	}
@@ -59,21 +50,37 @@ int chk_dmap (int n, char pos){
 	return dmap[n][y][x];
 }
 
-int pinp(char m){
+int plinp(char m){
 	switch(m){
-	case 'a':
-	case 's':
+		case 'w':
+		py--;
+		break;
+		
+		case 's':
+		py++;
+		break;
+		
+		case 'a':
+		px--;
+		break;
+		
+		case 'd':
+		px++;
+		break;
 	}
-	return imap[0][py][px];
+	return 0;
 }
 
 int main(){
 	initscr();
 	noecho();
-	while((ch=getch())!= 'q'){
-		p_test_imap();
-		getch();
+	char ch;
+	while((ch=getch())!='q'){
 		erase();
+		printw("Ughdhsf");
+		plinp(ch);
+		p_test_imap();
+		mvprintw(py,px,"@");
 	}
 	endwin();
 	return 0;
